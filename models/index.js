721e030ede79b1fccc3851da0cffaf0e5b0b2426
Jaron <jjkenyon777@gmail.com>
2021-11-02 18:37:06 -0700

    Models & associations

--- models/index.js
+++ models/index.js
@@ -1,1 +1,34 @@
-// model association here
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Category = require('./Category');
+
+Post.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+Category.hasMany(Post, {
+    foreignKey: 'category_id'
+});
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment, Category };
