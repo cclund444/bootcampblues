1dbf226f45df525118e7285486234992f75b3969
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-11-06 10:57:41 -0700

    set up for front end public routes. Theses may change but I wanted to have a basic set up done

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,1 +1,12 @@
 // public routes will be extended through this for front end apis 
+const router = require('express').Router();
+
+//calling public routes
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
+// using routes and dircting their url path
+router.use('/', homeRoutes);
+router.use('/dashbaord', dashboardRoutes);
+
+module.exports = router;
