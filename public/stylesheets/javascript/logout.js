848972c5589d00ec58217e4ce6d118707013d0be
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-11-09 11:10:16 -0800

    temp fill of login.js and logout.js

--- public/stylesheets/javascript/logout.js
+++ public/stylesheets/javascript/logout.js
@@ -1,1 +1,16 @@
 // logout front end javascript code will go in here
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+// selector will change to match our html
+document.querySelector('#logout').addEventListener('click', logout);
