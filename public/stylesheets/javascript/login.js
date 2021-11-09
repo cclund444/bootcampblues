01012bc427e94417c855b20ad7d8f54cabeadcc3
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-11-09 11:08:01 -0800

    temp fill of edit-post.js

--- public/stylesheets/javascript/login.js
+++ public/stylesheets/javascript/login.js
@@ -1,1 +1,59 @@
 // login front end javascript code will go in here
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    // selectors will change to match our html
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            console.log(response);
+            document.location.replace('/dashboard/');
+        } else {
+            console.log(response);
+            alert(response.statusText);
+        }
+    }
+}
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    // selectors will change to match our html
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            console.log(response);
+            document.location.replace('/dashboard/');
+        } else {
+            console.log(response);
+            alert(response.statusText);
+        }
+    }
+}
+
+// selectors will change to match our html
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
