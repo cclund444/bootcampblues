73de6ce7362bd986c35d57ae0be096b17dda6e75
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-11-09 11:08:59 -0800

    temp fill of and some editing to edit-post.js

--- public/stylesheets/javascript/login.js
+++ public/stylesheets/javascript/login.js
@@ -3,14 +3,14 @@ async function loginFormHandler(event) {
     event.preventDefault();
 
     // selectors will change to match our html
-    const username = document.querySelector('#username-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
-                username,
+                email,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
